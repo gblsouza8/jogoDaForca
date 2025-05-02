9ee5c0e0c8445ac221db9eb864637d4752dd89ac
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-05-02 08:53:57 -0300

    3º Commit

--- jogo.py
+++ jogo.py
@@ -1,3 +1,23 @@
 import random
 listaPalavras = ["banana", "abacate", "laranja", "uva", "melancia", "abacaxi", "morango"]
 tamanhoLista = len(listaPalavras)
+
+def gerarPalavra(tamanhoLista):
+    numero_aleatorio = random.randint(0, tamanhoLista)
+    palavra = listaPalavras[numero_aleatorio]
+    return palavra
+
+def criptografarPalavra(palavra):
+    palavraCriptografada = ""
+    tamanhoPalavra = len(palavra)
+    for i in range(tamanhoPalavra):
+        palavraCriptografada = palavraCriptografada + "_"
+    return palavraCriptografada
+
+
+
+
+
+
+
+
