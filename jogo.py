7777fd711411ab1b67c6210c47b96d8a19976a6c
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-05-02 09:35:02 -0300

    4º Commit

--- jogo.py
+++ jogo.py
@@ -1,6 +1,7 @@
 import random
 listaPalavras = ["banana", "abacate", "laranja", "uva", "melancia", "abacaxi", "morango"]
 tamanhoLista = len(listaPalavras)
+erros = 0
 
 def gerarPalavra(tamanhoLista):
     numero_aleatorio = random.randint(0, tamanhoLista)
@@ -13,6 +14,47 @@ def criptografarPalavra(palavra):
     for i in range(tamanhoPalavra):
         palavraCriptografada = palavraCriptografada + "_"
     return palavraCriptografada
+
+def pedirLetra(palavra, palavraCriptografada, erros):
+    letra = input("Insira uma letra: ")
+    acerto = 0
+
+    for i in range(len(palavra)):
+        
+        if palavra[i] == letra:
+                acerto = 1
+                lista_palavra = list(palavraCriptografada)
+                lista_palavra[i] = letra
+                palavraCriptografada = ''.join(lista_palavra)
+
+
+
+    if acerto > 0:
+        print("Você adivinhou uma letra!")
+    else:
+        print("Você errou... perdeu uma vida")
+        erros = erros + 1
+        print("Tentativas restantes:", 5-erros)
+    return palavraCriptografada, erros
+
+palavra = gerarPalavra(tamanhoLista)
+palavraCriptografada = criptografarPalavra(palavra)
+print(palavra)
+print(palavraCriptografada)
+
+
+while erros < 5:
+    palavraCriptografada, erros = pedirLetra(palavra, palavraCriptografada, erros)
+    print(palavraCriptografada)
+
+    if '_' not in palavraCriptografada:
+        print("Parabéns! você acertou todas as letras")
+        break
+
+
+
+if erros >= 5:
+    print("Você não conseguiu adivinhar a palavra... a palavra era: ", palavra)
 
 
 
